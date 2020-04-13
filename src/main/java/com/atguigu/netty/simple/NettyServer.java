c1ac683acec7dde05b2bf3766996fb6292892052
wanghao <wanghao19961026@163.com>
2020-04-13 20:22:13 +0800

    Future-Listener 机制

--- src/main/java/com/atguigu/netty/simple/NettyServer.java
+++ src/main/java/com/atguigu/netty/simple/NettyServer.java
@@ -1,10 +1,7 @@
 package com.atguigu.netty.simple;
 
 import io.netty.bootstrap.ServerBootstrap;
-import io.netty.channel.ChannelFuture;
-import io.netty.channel.ChannelInitializer;
-import io.netty.channel.ChannelOption;
-import io.netty.channel.EventLoopGroup;
+import io.netty.channel.*;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
@@ -43,6 +40,17 @@ public class NettyServer {
 
             //绑定一个端口并且同步。生成了一个ChannelFuture对象。启动服务器并绑定端口
             ChannelFuture cf = bootstrap.bind(6668).sync();
+            //给cf注册监听器，监控我们关心的事件
+            cf.addListener(new ChannelFutureListener() {
+                @Override
+                public void operationComplete(ChannelFuture channelFuture) throws Exception {
+                    if (cf.isSuccess()){
+                        System.out.println("监听端口6668成功");
+                    }else {
+                        System.out.println("监听端口失败");
+                    }
+                }
+            });
 
             //对关闭通道进行监听
             cf.channel().closeFuture().sync();
